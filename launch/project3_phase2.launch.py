26bc9358747cdf1f9cdb67db83400958dcad52a1
Iaryan-21 <86413005+Iaryan-21@users.noreply.github.com>
2024-04-04 14:29:55 -0400

    Updated Launch file with inputs via CLI

--- launch/project3_phase2.launch.py
+++ launch/project3_phase2.launch.py
@@ -15,8 +15,12 @@ def generate_launch_description():
     cmd_line_parameter = DeclareLaunchArgument('cmd_line_parameter', default_value='default_value', description='Description of the command line parameter')
     pkg_gazebo_ros = FindPackageShare('gazebo_ros')
 
-    x_pose = LaunchConfiguration('x_pose', default='0.0')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
+    declare_x_pose_arg = DeclareLaunchArgument('x_pose', default_value='0.0',description='Initial x pose')
+    declare_y_pose_arg = DeclareLaunchArgument('y_pose', default_value='0.0',description='Initial y pose')
+
+
+    x_pose = LaunchConfiguration('x_pose')
+    y_pose = LaunchConfiguration('y_pose')
     a_star_controller_node = Node(
         package='project3_phase2',
         executable='a_star_controller',
@@ -25,6 +29,11 @@ def generate_launch_description():
         parameters=[{'x_pose': x_pose, 'y_pose': y_pose}]
     )
 
+
+    ld.add_action(declare_x_pose_arg)
+    ld.add_action(declare_y_pose_arg)
     ld.add_action(cmd_line_parameter)
 
     return ld
+
+#to run ros2 launch turtlebot3_project3 project3_phase2.launch.py x_pose:=1.0 y_pose:=2.0
